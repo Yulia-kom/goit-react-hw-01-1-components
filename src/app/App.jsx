cfa216861fb94cef5b8f986eff3d9ca91b902b23
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2022-06-01 12:19:16 +0300

    profile add

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,16 @@
+import Profile from '../components/Profile';
+import user from '../user.json';
 
-function App() {
+export default function App() {
   return (
     <div>
-
-
-
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
-  )
+  );
 }
-
-export default App;
